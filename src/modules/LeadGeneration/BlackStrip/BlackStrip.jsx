2849fba790d7daec9a7b2833cb91b4136c246b94
papashango13 <vansh.pandita@appinventiv.com>
2024-10-07 13:36:40 +0530

    Update BlackStrip.jsx

--- src/modules/LeadGeneration/BlackStrip/BlackStrip.jsx
+++ src/modules/LeadGeneration/BlackStrip/BlackStrip.jsx
@@ -5,7 +5,7 @@ import BlackStripData from '../../../utils/data/BlackStrip.json'
 const BlackStrip = () => {
   return (
         <div className="image-gallery">
-      {BlackStripData.map((image) => (
+      {BlackStripData.map((image,index) => (
         <div key={image.id} className="image-container">
           <img src={image.img} />
         </div>
